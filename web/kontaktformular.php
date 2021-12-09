c59b2650d09d08c6d02aea8401374b295bb9198a
Nick Camenisch <camenischn@bzz.ch>
2021-12-09 14:31:27 +0100

    nachrichtenfeld eingefügt und über inhalt überprüft

--- web/kontaktformular.php
+++ web/kontaktformular.php
@@ -46,9 +46,9 @@ header('Content-Type: text/html; charset=utf-8');
     <div class="formular">
       <?php
       //Initialisierung der Variabeln
-      $nameErr = $vornameErr = $emailErr = $newsletterErr = $infoErr = $zahlungErr = "";
-      $name = $vorname = $email = $newsletter = $info = $zahlung = $ausgabe = "";
-      $titel = "Bestellformular";
+      $nameErr = $vornameErr = $emailErr = $nachrichtErr = "";
+      $name = $vorname = $email = $nachricht = $ausgabe = "";
+      $titel = "Kontakt";
       //Wenn senden gedrückt wurde
       if (isset($_POST['senden'])) {
         //Name überprüfung
@@ -71,9 +71,15 @@ header('Content-Type: text/html; charset=utf-8');
             $vornameErr = "Nur Buchstaben sind erlaubt.";
           }
         }
+        //Nachrichtenfeld überprüfung
+        if (empty($_POST["nachricht"])) {
+          $nachrichtErr = "Bitte geben sie eine Nachricht ein.";
+        } else {
+          $nachricht = test_input($_POST["nachricht"]);
+        }
         //Email überprüfung
         if (empty($_POST["email"])) {
-          $emailErr = "Eine Email wird benötigt.";
+          $emailErr = "Bitte geben sie eine verfügbare Email ein.";
         } else {
           $email = test_input($_POST["email"]);
           //Auf RegEx kontrolieren
@@ -81,23 +87,7 @@ header('Content-Type: text/html; charset=utf-8');
             $emailErr = "Geben sie eine formal gerechte email ein.";
           }
         }
-        //Wenn leer dann fehlerausgabe sonst input verändern um cross script zu verhindern
-        if (empty($_POST["newsletter"])) {
-          $newsletterErr = "Bitte eine Eingabe betätigen";
-        } else {
-          $newsletter = test_input($_POST["newsletter"]);
-        }
-        if (empty($_POST["info"])) {
-          $infoErr = "Bitte eine Eingabe betätigen";
-          $info = '';
-        } else {
-          $info = test_input(implode(($_POST["info"]))); //Implode, da Array
-        }
-        if ($_POST['zahlung'] === '') {
-          $zahlungErr = "Bitte wählen sie eine Zahlungsmöglichkeit aus.";
-        } else {
-          $zahlung = test_input($_POST['zahlung']);
-        }
+
       }
       //Testet auf Cross Scripting
       function test_input($data)
@@ -108,14 +98,11 @@ header('Content-Type: text/html; charset=utf-8');
         return $data;
       }
        //Bestellung ausgeben
-       if ($nameErr === '' && $vornameErr === '' && $emailErr === '' && $infoErr === '' && $newsletterErr === '' && $zahlungErr === '' && isset($_POST['senden'])) {
-        $ausgabe = "Name: $name <br> Vorname: $vorname <br> Email: $email <br> Info: $info <br> Zahlungsart: $zahlung <br> Newsletter: $newsletter <br>";
+       if ($nameErr === '' && $vornameErr === '' && $emailErr === '' && $nachrichtErr === '' && isset($_POST['senden'])) {
+        $ausgabe = "Name: $name <br> Vorname: $vorname <br> Email: $email <br> Nachricht: $nachricht";
         $name = "";
         $vorname = "";
-        $email = "";
-        $info = "";
-        $newsletter = "";
-        $zahlung = "";
+        $nachricht = "";
       }
       //Änderung der Hintergrundfarbe nach Auswahl des Users
 
@@ -126,7 +113,7 @@ header('Content-Type: text/html; charset=utf-8');
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" accept-charset="utf-8">
           <!-- Erstellt die Einzelnen Eingaben -->
           <label>Name
-            <input  type="text" id="name" class="name" name="name" value="<?php echo $name ?>" placeholder="" s><span class="error">* <?php echo $nameErr; ?></span>
+            <input  type="text" id="name"  name="name" value="<?php echo $name ?>" placeholder="" s><span class="error">* <?php echo $nameErr; ?></span>
           </label><br><br><br>
           <label>Vorname
             <input type="text" id="vorname" name="vorname" value="<?php echo $vorname ?>" placeholder=""><span class="error">* <?php echo $vornameErr; ?></span>
@@ -134,60 +121,19 @@ header('Content-Type: text/html; charset=utf-8');
           <label>Email
             <input type="email" id="email" name="email" value="<?php echo $email ?>" placeholder=""><span class="error">* <?php echo $emailErr; ?></span>
           </label><br><br><br>
-          <fieldset>
-            <legend>Ich bestelle Informationen zu<span class="error">* <?php echo $infoErr; ?></span></legend>
-            <label>
-              <input type="checkbox" name="info[]" value="PHP" <?php if (strstr($info, 'PHP')) { ?> checked <?php } ?>>PHP
-            </label><br>
-            <label>
-              <input type="checkbox" name="info[]" value="JavaScript" <?php if (strstr($info, 'JavaScript')) { ?> checked <?php } ?>>JavaScript
-            </label><br>
-            <label>
-              <input type="checkbox" name="info[]" value="CSS" <?php if (strstr($info, 'CSS')) { ?> checked <?php } ?>>CSS
-            </label>
-          </fieldset><br>
-          <fieldset>
-            <legend>Zahlungsmethode<span class="error">* <?php echo $zahlungErr; ?></span></legend>
-            <select name="zahlung">
-              <option value="">Bitte auswählen</option>
-              <option value="Barzahlung" <?php if ($zahlung == "Barzahlung") { ?> selected <?php  } ?>>Barzahlung</option>
-              <option value="Rechnung" <?php if ($zahlung == "Rechnung") { ?> selected <?php  } ?>>Rechnung</option>
-              <option value="Sofortüberweisung" <?php if ($zahlung == "Sofortüberweisung") { ?> selected <?php  } ?>>Sofortüberweisung</option>
-              <option value="VISA" <?php if ($zahlung == "VISA") { ?> selected <?php  } ?>>VISA</option>
-              <option value="PayPal" <?php if ($zahlung == "PayPal") { ?> selected <?php  } ?>>PayPal</option>
-              <option value="PaySafeCard" <?php if ($zahlung == "PaySafeCard") { ?> selected <?php  } ?>>PaySafeCard</option>
-              <option value="Bitcoin" <?php if ($zahlung == "Bitcoin") { ?> selected <?php  } ?>>Bitcoin</option>
-            </select>
-          </fieldset><br>
-          <fieldset>
-            <legend>Ich abboniere den Newsletter<span class="error">* <?php echo $newsletterErr; ?></span></legend>
-            <label>
-              <input type="radio" id="ja" <?php if (isset($newsletter) && $newsletter == "Ja") echo "checked"; ?> name="newsletter" value="Ja">Ja
-            </label><br>
-            <label>
-              <input type="radio" id="search" <?php if (isset($newsletter) && $newsletter == "Nein") echo "checked"; ?> name="newsletter" value="Nein">Nein
-            </label>
-          </fieldset>
-          <!--
-          <fieldset>
-            <legend>File</legend>
-            <label>
-              <form action="/action_page.php">
-                <input type="file" id="myFile" name="filename">
-                <input type="submit">
-              </form>
-            </label>
-          </fieldset> -->
+          <label>Nachricht*
+            <textarea name="nachricht" id="nachricht" name="nachricht" cols="50" rows="6" value="<?php echo $nachricht ?>"></textarea><span class="error">*<?php echo $nachrichtErr; ?></span>
+          </label>
           <br><br>
           <!--Absenden-->
-          <input type="submit" name="senden" value="senden">
+          <input id="submit" type="submit" name="senden" value="senden">
           <div class="loader"></div>
         </form>
       </div>
     </div>
     <div class="bestellung">
       <?php
-      if ($nameErr === '' && $vornameErr === '' && $emailErr === '' && $infoErr === '' && $newsletterErr === '' && $zahlungErr === '' && isset($_POST['senden'])) {
+      if ($nameErr === '' && $vornameErr === '' && $emailErr === '' && isset($_POST['senden'])) {
         echo "<style>.formular { display:none; }</style>"; //Formular ausblenden
         echo "<h2>Bestellung</h2>";
         echo $ausgabe;
